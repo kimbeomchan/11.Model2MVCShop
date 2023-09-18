0d97ff1bbfb9ffb5efcb3e86850cdf8e177c91d2
Beomchan Kim <bmcnkim@gmail.com>
2023-09-18 23:59:08 +0900

    Create:2023.09.18:Spring Boot
    Model2MVCShop
    JavaEE to Spring Boot 변환-

---
+++ src/main/java/com/model2/mvc/service/purchase/impl/PurchaseDaoImpl.java
@@ -0,0 +1,68 @@
+package com.model2.mvc.service.purchase.impl;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.purchase.PurchaseDao;
+import com.model2.mvc.service.domain.Purchase;
+
+@Repository("purchaseDaoImpl")
+public class PurchaseDaoImpl implements PurchaseDao {
+
+	///Field
+	@Autowired
+	@Qualifier("sqlSessionTemplate")
+	private SqlSession sqlSession;
+
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	///Constructor
+	public PurchaseDaoImpl() {
+		System.out.println(this.getClass());
+	}
+
+	///Method	
+	public Purchase findPurchase(int tranNo) throws Exception{
+		return sqlSession.selectOne("PurchaseMapper.getPurchase", tranNo);
+	}
+
+
+	public List<Purchase> getPurchaseList(Search search) throws Exception{
+		return sqlSession.selectList("PurchaseMapper.getPurchaseList", search);
+	}
+
+
+	public void insertPurchase(Purchase purchase) throws Exception{
+		sqlSession.insert("PurchaseMapper.addPurchase", purchase);
+	}
+
+	public void updatePurchase(Purchase purchase) throws Exception {
+		sqlSession.update("PurchaseMapper.updatePurchase", purchase);
+	}
+
+
+
+	public void updateTranCode(Purchase purchase) throws Exception {
+		sqlSession.update("PurchaseMapper.updateTranCode", purchase);
+	}
+
+
+	// 게시판 Page 처리를 위한 전체 Row(totalCount)  return
+	public int getTotalCount(Search search) throws Exception {
+		return sqlSession.selectOne("PurchaseMapper.getTotalCount", search);
+	}
+
+
+	public HashMap<String, Object> getSaleList(Search search) throws Exception{
+		return null;
+	}
+
+}//end of class
